e0991412870f39d1a57cf92782d26f1341ac52d6
zhaozhenhua <zhaozhenhua@bxgcb.com>
2019-11-20 00:17:42 +0800

    name_main

---
+++ choose.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3cat
+#_*_conding:utf-8_*_
+
+#print('hello,world')
+#print('hello,shiyanlou')
+if __name__ == '_main_':
+	print('hello,world')
+	print('hello,shiyanlou')
+else:
+    print('hello,world')
