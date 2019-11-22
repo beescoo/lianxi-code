019898ed53e177aec188b3d09f313b3d6eafca7d
zhaozhenhua <zhaozhenhua@bxgcb.com>
2019-11-22 23:16:45 +0800

    3hours

---
+++ ditctest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env.python3
+
+import sys
+#x = {}
+for a in sys.argv[1:]:
+    a_list = a.split(':')
+    a_tuple = ('H','Z')
+    'IDa_list[0]','Name:a_list[1]' = a_tuple
+    x = {}
+    x[H] = Z
+    for b in x:
+        print(b)
